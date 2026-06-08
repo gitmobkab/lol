d2dc55a0d30206b14ee5c9ac78168c601f11db1d
gitmobkab <richmondaurel77@gmail.com>
2026-06-08 14:23:14 +0000

    feat: Add build data and versioning functions for application metadata

---
+++ data/build_data.go
@@ -0,0 +1,29 @@
+package data
+
+import "fmt"
+
+var (
+	Version string = "dev"
+	BuildTime string = "local"
+	CommitHash string = "dirty"
+)
+
+/* 
+returns a formatted version string with build information,
+the format is:
+
+V<Version> (<OS>/<ARCH>)
+*/
+func GetVersion() string {
+	return fmt.Sprintf("V%s (%s/%s)", Version, OS, ARCH)
+}
+
+/* 
+returns a more detailed formatted version string with build information,
+the format is:
+
+V<Version> (<OS>/<ARCH>) [<BuildTime>] *<CommitHash>
+*/
+func GetDetailedVersion() string {
+	return fmt.Sprintf("V%s (%s/%s) [%s] *%s", Version, OS, ARCH, BuildTime, CommitHash)
+}
---
+++ data/data.go
@@ -0,0 +1,8 @@
+package data
+
+import "runtime"
+
+const AppName string = "lol"
+
+const OS string = runtime.GOOS
+const ARCH string = runtime.GOARCH
